5210dbc4dd60a50098fd08838cfedf28399c3d88
imzhyke <punk1313444@gmail.com>
2025-02-10 23:57:28 +0800

    Project Initialization
    Expo Router@lates w/ nativewind ready

---
+++ global.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="nativewind/types" />
+declare module "*.png" {
+  const content: {
+    uri: string;
+    width: number;
+    height: number;
+    [key: string]: any; // For additional properties Expo might add
+  };
+  export default content;
+}
+
+  
+  declare module "*.jpg" {
+    const content: string;
+    export default content;
+  }
+  
+  declare module "*.jpeg" {
+    const content: string;
+    export default content;
+  }
+  
+  declare module "*.svg" {
+    const content: string;
+    export default content;
+  }
+  
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-slate-300 h-full w-full justify-center items-center">
+      <Text className="text-black text-2xl">Hello World!</Text>
+    </View>
+  );
+}
